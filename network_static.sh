a6ec8631d3fc86c706bd66ed4b170494b2c2830d
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-02 11:43:33 +0300

    Create network_static.sh

---
+++ network_static.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIR='/etc/netplan/'
+DIR1='/home/otus-app3/03-static.yaml'
+DIR2='/etc/netplan/'
+
+#Копируем конфиг 
+sudo -s
+cp $DIR1 $DIR2
+echo "файл конфига 03-static.yaml скопирован в $DIR"
+
+netplan generate
+netplan apply
+if [ $? -eq 0 ]; then
+          echo "Статический IP включен"
+else
+          echo "Возникла ошибка"
+fi
+read -p "Для просмотра сетевых интерфейсов нажмите Enter"
+ip -4 a
