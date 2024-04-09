1d0b8f45f66e76486307209c3058d19a63c14e1d
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-09 12:45:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,23 @@
-#!/bin/bash
+#!/bin/bash  elasticsearch.yml
 
-DIR='/etc/nginx/sites-enabled/'
-DIR1='/home/otus-app1/default'
-DIR3='/home/otus-app1/status.conf'
-DIR2='/etc/nginx/sites-enabled/'
+DIR='/etc/elasticsearch/'
+DIR1='/home/otus-app3/elasticsearch.yml'
+#DIR3='/home/otus-app1/status.conf'
+#DIR2='/etc/nginx/sites-enabled/'
 
 #Ограничение потребления java
 echo -e "-Xms1g\n-Xmx1g"> /etc/elasticsearch/jvm.options.d/jvm.options
 echo "Java ограничен в потребление ресурсов, 1 gb"
+
+#Скопировать файл конфигурации 
+cp $DIR1 $DIR
+systemctl daemon-reload
+systemctl restart elasticsearch
+if [ $? -eq 0 ]; then
+          echo "elasticsearch запущен, готов к работе"
+else
+          echo "Возникла ошибка, elasticsearch"
+fi
 
 
 
