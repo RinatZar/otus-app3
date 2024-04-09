9826f5f17194bc96eb0ac41357b0f92d4ef1a980
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-09 13:47:27 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -35,11 +35,18 @@ else
 fi
 
 # Настройка logstash
-if [ -f "/etc/logstash/logstash.yml" ]; then
-  sed -i "s/^# path.config:\s*:/path.config: /etc/logstash/conf.d/" "/etc/logstash/logstash.yml"
-  else
-  echo "файл /etc/logstash/logstash.yml не найден"
+
+file="/etc/logstash/logstash.yml"
+search_string="# path.config:"
+replace_string="path.config: /etc/logstash/conf.d"
+
+if grep -q "$search_string" "$file"; then
+    awk -v search="$search_string" -v replace="$replace_string" '{gsub(search, replace)} 1' "$file" > temp_file && mv temp_file "$file"
+    echo "Строка '$search_string' успешно заменена на '$replace_string' в файле $file"
+else
+    echo "Строка '$search_string' не найдена в файле $file"
 fi
+
 systemctl daemon-reload
 systemctl restart logstash
 if [ $? -eq 0 ]; then
