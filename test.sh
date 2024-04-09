113d260e3066146e5f5c3a6129521e36a0fe0dbc
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-09 13:42:40 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ search_string="# path.config:"
 replace_string="path.config: /etc/logstash/conf.d"
 
 if grep -q "$search_string" "$file"; then
-    sed -i "s/$search_string/$replace_string/g" "$file"
+    awk -v search="$search_string" -v replace="$replace_string" '{gsub(search, replace)} 1' "$file" > temp_file && mv temp_file "$file"
     echo "Строка '$search_string' успешно заменена на '$replace_string' в файле $file"
 else
     echo "Строка '$search_string' не найдена в файле $file"
