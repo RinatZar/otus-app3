c7277cc5c64cc023b7ab6906a8a32ead04ddccf2
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-09 13:20:02 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,9 +26,27 @@ if [ -f "/etc/kibana/kibana.yml" ]; then
 else
   echo "файл /etc/kibana/kibana.yml не найден"
 fi
-#service mysql restart
-#echo "Конфиг MySQL-source настроен"
+systemctl daemon-reload
+systemctl restart kibana
+if [ $? -eq 0 ]; then
+          echo "kibana запущен, готов к работе"
+else
+          echo "Возникла ошибка, kibana"
+fi
 
+# Настройка logstash
+if [ -f "/etc/logstash/logstash.yml" ]; then
+  sed -i "s/^# path.config:\s*:/path.config: /etc/logstash/conf.d/" "/etc/logstash/logstash.yml"
+  else
+  echo "файл /etc/logstash/logstash.yml не найден"
+fi
+systemctl daemon-reload
+systemctl restart logstash
+if [ $? -eq 0 ]; then
+          echo "logstash запущен, готов к работе"
+else
+          echo "Возникла ошибка, logstash"
+fi
 
 
 
