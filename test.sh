678f4d5f5c632e27f52a8fea5b4bb8d93919eb90
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-09 13:25:10 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 
 # Настройка logstash
 if [ -f "/etc/logstash/logstash.yml" ]; then
-  sed -i "s/^# path.config:\s*:/path.config: /etc/logstash/conf.d/" "/etc/logstash/logstash.yml"
+  sed -i "s/^# path.config:\s*/path.config: /etc/logstash/conf.d/" "/etc/logstash/logstash.yml"
+#  sed -i "s/^bind-address\s*=\s*127.0.0.1/$bind_address_new/" $cre_mysqld_cnf
   else
   echo "файл /etc/logstash/logstash.yml не найден"
 fi
