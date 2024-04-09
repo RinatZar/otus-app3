360aa0d48eab384fd4ea6dc80e1ae685c2616522
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-09 13:21:50 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+# Настройка logstash
+if [ -f "/etc/logstash/logstash.yml" ]; then
+  sed -i "s/^# path.config:\s*:/path.config: /etc/logstash/conf.d/" "/etc/logstash/logstash.yml"
+  else
+  echo "файл /etc/logstash/logstash.yml не найден"
+fi
+systemctl daemon-reload
+systemctl restart logstash
+if [ $? -eq 0 ]; then
+          echo "logstash запущен, готов к работе"
+else
+          echo "Возникла ошибка, logstash"
+fi
