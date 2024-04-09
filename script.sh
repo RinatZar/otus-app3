4405f3d665456788cd14767a4fec2853589baa60
RinatZar <103266528+RinatZar@users.noreply.github.com>
2024-04-09 11:15:28 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,19 +6,21 @@ DIR3='/home/otus-app1/status.conf'
 DIR2='/etc/nginx/sites-enabled/'
 
 #Ограничение потребления java
-cat > /etc
+cat > /etc/elasticsearch/jvm.options.d/jvm.options
+-Xms1g
+-Xmx1g
 
 
 
 
 #Копируем конфиг 
-cp $DIR1 $DIR2
-cp $DIR3 $DIR2
-echo "файл конфиги скопированы в $DIR"
-systemctl restart nginx
-systemctl restart prometheus-nginx-exporter
-if [ $? -eq 0 ]; then
-          echo "Nginx запущен, готов к работе"
-else
-          echo "Возникла ошибка"
-fi
+#cp $DIR1 $DIR2
+#cp $DIR3 $DIR2
+#echo "файл конфиги скопированы в $DIR"
+#systemctl restart nginx
+#systemctl restart prometheus-nginx-exporter
+#if [ $? -eq 0 ]; then
+#          echo "Nginx запущен, готов к работе"
+#else
+#          echo "Возникла ошибка"
+#fi
